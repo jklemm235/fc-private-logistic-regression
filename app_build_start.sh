1200ea0f1828989738b756831f2e62f799a5e316
Julian Klemm <julian.klemm1@gmail.com>
2022-08-04 19:46:22 +0200

    init

--- app_build_start.sh
+++ app_build_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 featurecloud app build
-featurecloud test start --controller-host=http://localhost:8000 --client-dirs=./client_1,./client_2 --generic-dir=./ --app-image=fc-private-logistic-regression --channel=local --query-interval=2 --download-results=./output
+featurecloud test start --controller-host=http://localhost:8002 --client-dirs=./client_1,./client_2 --generic-dir=./ --app-image=fc-private-logistic-regression --channel=local --query-interval=2 --download-results=./output
