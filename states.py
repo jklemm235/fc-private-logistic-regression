4130bca972c8586da8413a5324285f224741d9d4
Julian Klemm <julian.klemm1@gmail.com>
2022-08-22 18:23:08 +0200

    Removed comments, added LabelColumn to the config file

--- states.py
+++ states.py
@@ -32,56 +32,31 @@ class InitialState(AppState):
         self.register_transition("local_computation", Role.BOTH)
 
     def run(self):
-        # load in config self.internal["epsilon", "delta", ...]
-        #TODO: remove this
-        self.log("Init fc-private-logistic-regression start", level = LogLevel.DEBUG)
-        print("Inital state start")
+        ### load in config self.internal["epsilon", "delta", ...]
+        if os.path.exists(os.path.join(os.getcwd(), "mnt", "input",
+                                       "config.yaml")):
+            configFileName = "config.yaml"
+        elif os.path.exists(os.path.join(os.getcwd(), "mnt", "input",
+                                         "config.yml")):
+            configFileName = "config.yml"
+        else:
+            self.log("Error, no config.yaml file found, aborting",
+                     level = LogLevel.FATAL)
+
         try:
-            #print(os.listdir(os.getcwd()))
-            #print('Home contains:')
-            #print(os.listdir(
-            #    os.path.join(os.getcwd(), "home")))
-            #print('mnt/input contains:')
-            #print(os.listdir(
-            #    os.path.join(os.getcwd(), "mnt", "input")))
-            #TODO: remove prints
-            with open(os.path.join(os.getcwd(), "mnt", "input", "config.yaml"), 'r') as stream:
+            with open(os.path.join(os.getcwd(), "mnt", "input",
+                                   configFileName), 'r') as stream:
                 config = yaml.safe_load(stream)
-                print(config) #TODO: rmv
-                #TODO: fix open to work without the hack of copying everything to
-                # data
         except Exception as err:
-            self.log(f"Error loading config.yaml: f{err}" , level = LogLevel.FATAL)
+            self.log(f"Error loading config.yaml: f{err}",
+                     level = LogLevel.FATAL)
 
         self.store(key = "config", value = config)
 
-        # load in data in self.internal["data"]
-        #TODO: fix so it always loads data correctly when it is known how docker works
-        train_data = pd.read_csv(os.path.join(os.getcwd(), "mnt", "input","training_set.csv"), index_col=0)
-        test_data = pd.read_csv(os.path.join(os.getcwd(), "mnt", "input","test_set.csv"), index_col=0)
-
-        X_train = np.array(train_data.drop(columns=['Chance']))
-        X = np.array(X_train)
-        y_train = np.array(train_data['Chance'])
-        print("Shape of loaded data is:") #TODO: rmv
-        print(X.shape) #TODO: rmv
-        print(y_train.shape) #TODO: rmv
-        # not put in dict yet, they will be modified first by the LogisticRegression_DPSGD class
-
-        X_test = np.array(test_data.drop(columns=['Chance']))
-        y_test = np.array(test_data['Chance'])
-        self.store(key = "X_test", value = X_test)
-        self.store(key = "y_test", value = y_test)
-
-        # load in weights and other parameters
-        n, d = X.shape
-        self.store(key = "n", value = n)
-        self.store(key = "d", value = d) #TODO: works if just one row in predictions
-                # are there models that predict n values and therefore n cols?
         if self.is_coordinator:
             self.store(key = "cur_communication_round", value = 0)
 
-
+        ### SGD Class creation
         DPSGD_class = algo.LogisticRegression_DPSGD()
         # DP information
         if "dpSgd" in config["dpMode"]:
@@ -89,7 +64,6 @@ class InitialState(AppState):
         else:
             DPSGD_class.DP = False
 
-        # SGD Class creation
         try:
             DPSGD_class.alpha = config["sgdOptions"]["alpha"]
             DPSGD_class.max_iter = config["sgdOptions"]["max_iter"]
@@ -99,9 +73,44 @@ class InitialState(AppState):
             DPSGD_class.C = config["dpOptions"]["C"]
             DPSGD_class.epsilon = config["dpOptions"]["epsilon"]
             DPSGD_class.delta = config["dpOptions"]["epsilon"]
+            labelCol = config["labelColumn"]
         except Exception as err:
             self.log(f"Config file seems to miss fields: {err}")
 
+
+        ### Load in Data
+
+        # check if data files exist
+        if not os.path.exists(os.path.join(os.getcwd(), "mnt",
+                                           "input","training_set.csv")):
+            self.log("Could not find training_set.csv containing training data",
+                        level = LogLevel.FATAL)
+        if not os.path.exists(os.path.join(os.getcwd(), "mnt",
+                                           "input","test_set.csv")):
+            self.log("Could not find training_set.csv containing test data",
+                        level = LogLevel.FATAL)
+
+        # load in data
+        train_data = pd.read_csv(os.path.join(os.getcwd(), "mnt", "input","training_set.csv"), index_col=0)
+        test_data = pd.read_csv(os.path.join(os.getcwd(), "mnt", "input","test_set.csv"), index_col=0)
+
+        # preprocess data
+        X_train = np.array(train_data.drop(columns=[labelCol]))
+        X = np.array(X_train)
+        y_train = np.array(train_data[labelCol])
+
+        X_test = np.array(test_data.drop(columns=[labelCol]))
+        y_test = np.array(test_data[labelCol])
+        self.store(key = "X_test", value = X_test)
+        self.store(key = "y_test", value = y_test)
+
+        # load in weights and other parameters
+        n, d = X.shape
+        self.store(key = "n", value = n)
+        self.store(key = "d", value = d)
+
+        # modify data depending on which prediction function is used
+        # (binary vs multiple classes)
         X, y_train = DPSGD_class.init_theta(X, y_train)
         self.store(key = "X", value = X)
         self.store(key = "y_train", value = y_train)
@@ -123,8 +132,6 @@ class obtainWeights(AppState):
         DPSGD_class = self.load("DPSGD_class")
         DPSGD_class.theta = self.await_data(n = 1, unwrap=True, is_json=False)
         self.store(key="DPSGD_class", value = DPSGD_class)
-        print(vars(DPSGD_class))
-        print("obtained weights: {}".format(DPSGD_class.theta))
         return "local_computation"
 
 @app_state("local_computation")
@@ -135,37 +142,33 @@ class localComputationState(AppState):
         self.register_transition("obtain_weights", Role.PARTICIPANT)
 
     def run(self):
-        random.seed(10) #TODO: remove this
         # Set parameter
         X = self.load("X")
         y = self.load("y_train")
         n = self.load("n")
         d = self.load("d")
 
-        "Train Logistic regression with SGD"
+        # Training
         DPSGD_class = self.load("DPSGD_class")
         DPSGD_class.train(X, y)
         self.store(key="DPSGD_class", value = DPSGD_class)
-        print("Local Training finished, updated class is:") #TODO rmv
-        print(vars(DPSGD_class)) #TODO rmv
-        print(DPSGD_class.theta.shape) #TODO: rmv
+
 
         # save theta of each client
         cur_computation_round = self.load("cur_computation_round") + 1
         self.store(key = "cur_computation_round", value = cur_computation_round)
         fp = open(os.path.join("mnt", "output", "model_{}_{}.pyc".format(
-            self.id, cur_computation_round)), 'wb')
+            self.id, cur_computation_round)), 'wb') #TODO: rmv in Master
         np.save(fp, DPSGD_class.theta)
 
         # local update
         if self.is_coordinator:
-            #TODO: add dp noise here possibly
-            self.send_data_to_coordinator(DPSGD_class.theta, send_to_self = True, use_smpc = False)
+            self.send_data_to_coordinator(DPSGD_class.theta,
+                                          send_to_self = True, use_smpc = False)
             return "aggregate_data"
         else:
-            self.send_data_to_coordinator(DPSGD_class.theta, send_to_self = False, use_smpc = False)
-            #TODO: possibly an infinite loop here? Or does the coordinator
-            # finnishing also terminate every client?
+            self.send_data_to_coordinator(DPSGD_class.theta,
+                                        send_to_self = False, use_smpc = False)
             return "obtain_weights"
 
 
@@ -179,33 +182,30 @@ class aggregateDataState(AppState):
         self.register_transition("terminal", Role.COORDINATOR)
 
     def run(self):
-        # TODO: how to manage coordinator adding noise, best add func in template
+        # aggregate the weights
         weights_updated = self.aggregate_data(use_smpc=False)
         weights_updated = weights_updated / len(self._app.clients)
-        print(weights_updated.shape) #TODO rmv
-        print("aggregated weights:") #TODO: remove prints
-        print(weights_updated) #TODO rmv
+
+        # update communication_rounds
         cur_comm = self.load("cur_communication_round") + 1
         self.store(key = "cur_communication_round",
                         value = cur_comm)
+
+        #TODO: rmv in master
         fp = open(os.path.join("mnt", "output", "aggmodel_{}.pyc".format(cur_comm)), "wb")
         np.save(fp, weights_updated)
-        print("cur_comm is {}".format(cur_comm))
-        print("max_comm is {}".format(self.load("config")["communication_rounds"]))
+
         if cur_comm >= self.load("config")["communication_rounds"]:
             # finnished
-            #TODO: safe result in file?
-            print("Done:")  #TODO: remove these lines
-            print(weights_updated) #TODO rmv
-            print(weights_updated.shape)
-            print("Shape of test data to be evaluated:") #TODO: rmv
-            print(self.load("X_test").shape) #TODO: rmv
-            print(self.load("y_test").shape) #TODO: rmv
-            #TODO send data to itself for adding dp necessary
+            fp = open(os.path.join("mnt", "output", "trained_model.pyc".format(cur_comm)), "wb")
+            np.save(fp, weights_updated)
+
+
+            #TODO: rmv in Master
             DPSGD_class = self.load("DPSGD_class")
             DPSGD_class.theta = weights_updated
             DPSGD_class.evaluate(X = self.load("X_test"), y = self.load("y_test"))
-            # save config (with numClients)
+            # save config (with numClients) #TODO: rmv in Master
             config = self.load("config")
             config["numClients"] = len(self.clients)
             fp = open(os.path.join("mnt", "output", "config.yaml"), "w")
@@ -214,8 +214,6 @@ class aggregateDataState(AppState):
         else:
             # send data to clients
             self.broadcast_data(weights_updated, send_to_self = True)
-            #TODO: careful, send_to_self just means the instance adds the weights
-            # to its own incoming vector
             return "obtain_weights"
 
 
