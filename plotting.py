a87fd0e24ca8a519c2729572ae4d039fec2b8a69
Klemm <bar7937@llaima.zbh.uni-hamburg.de>
2022-09-17 12:51:26 +0200

    Kick out older test runs that are not relevant anymore

--- plotting.py
+++ plotting.py
@@ -2,5 +2,9 @@ import pandas as pd
 dfRaw =  pd.read_csv("analysis.csv")
 df = dfRaw.drop(["foldPrivacytest", "noiseScale", "testnum"],
                 axis = 1)
-print(df.groupby(["dpMode", "epsilon", "numClients", "max_iter", "L", "communication_rounds", "alpha", "lambda_", "tolerance"])\
-    .agg({"accuracy":["mean", "std"], "tolerance": "count"}))
+index_names = df[(df['alpha'] != 0.1) | (df["lambda_"] != 0.01) | (df["tolerance"] != 0.00001) | (df["L"] != 1.0) ].index
+df.drop(index_names, inplace=True)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 120):  # more options can be specified also
+    print(df.groupby(["dpMode", "epsilon", "numClients", "max_iter", "L", 
+        "communication_rounds", "tolerance"])\
+                .agg({"accuracy":["mean", "std"], "tolerance": "count"}))
