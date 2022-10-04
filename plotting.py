366e220bf7cc51b02c92bb96e698bd78f8faddc2
Julian Klemm <julian.klemm1@gmail.com>
2022-10-05 01:31:45 +0200

    changed template to contain correct values

--- plotting.py
+++ plotting.py
@@ -5,33 +5,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv("processedAnalysis.csv")
+df = df.drop(df[(df["C"].isin([1, "1"])) & (df["max_iter"] == 2500) & \
+    (df["lambda_"] == 0.05) & (df["epsilon"] == 30) & \
+    (df["numClients"] == 5) & (df["communication_rounds"] == "1_1")].index) #invalid tests
+df = df.drop(df[df["alpha"] != 0.1].index) # old tests
+df = df.drop(df[df["C"] == '1.2'].index) # used invalid sensitivity
 
-'''
-with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 120):  # more options can be specified also
-    print(df.groupby(["dpMode", "max_iter", "epsilon", "L", "lambda_", "numClients",
-        "communication_rounds", "tolerance"])\
-                .agg({"accuracy":["mean", "std"], "tolerance": "count"}))
-'''
-#TODO: next part only temp
-
-data = df[(df["lambda_"].isin([0.05, 0.1])) & (df["numClients"].isin([5])) \
-        & (df["communication_rounds"] == 1) & (df["dpMode"].isin(["noDP",
-            "dpClient"])) & (df["max_iter"] >= 500)]
-data = data.drop(data[(data["dpMode"] == "dpClient") & (data["epsilon"] != 30)].index)
-
-print(data.groupby(["max_iter", "L", "alpha",
-                    "communication_rounds", "epsilon", "tolerance",
-                    "lambda_", "dpMode", "numClients"])\
-                .agg({"accuracy":["mean", "std"], "tolerance": "count"}))
-exit()
-#TODO: changing of labels, maybe of ticks, maybe add titles -> make everything prettier
-### Show that behaviour is worse, lambda can improve the behaviour
+### Difference when using noDP tuned parameters
 ### also shows numClients impact
 ### 1 communication round, 1,5,7,10 clients, 0.01 and 0.5 for lambda_, eps = 30
-'''
-data = df[(df["lambda_"].isin([0.01, 0.5])) & (df["numClients"].isin([1,3,5,7,10])) \
-        & (df["communication_rounds"] == 1) & (df["dpMode"].isin(["noDP",
-            "dpClient"])) & (df["max_iter"] == 500)]
+#'''
+data = df[(df["lambda_"].isin([0.01])) & (df["numClients"].isin([3,5,7])) \
+        & (df["communication_rounds"] == "1_1") & (df["dpMode"].isin(["noDP",
+            "dpClient"])) & (df["max_iter"].isin([500]))]
 data = data.drop(data[(data["dpMode"] == "dpClient") & (data["epsilon"] != 30)].index)
 
 print(data.groupby(["max_iter", "L", "alpha",
@@ -39,41 +25,16 @@ print(data.groupby(["max_iter", "L", "alpha",
                     "lambda_", "dpMode", "numClients"])\
                 .agg({"accuracy":["mean", "std"], "tolerance": "count"}))
 facetgrid = sns.catplot(x="numClients", y="accuracy", hue="dpMode",
-                        kind="box", col="lambda_", data=data)
+                        kind="box", data=data)
 fig = facetgrid.figure
-fig.savefig("noDP_vs_dpClient_lowHigh_Lambda.png")
-'''
-
-### show where variance comes from
-### 1 communication round, 5 clients, 0.5 for lambda_, eps = 30, all Folds
-'''
-data = df[(df["lambda_"].isin([0.5])) & (df["numClients"].isin([5])) \
-        & (df["communication_rounds"] == 1) & (df["dpMode"].isin(["noDP",
-            "dpClient"])) & (df["max_iter"].isin([500, 2500]))]
-data = data.drop(data[(data["dpMode"] == "dpClient") & (data["epsilon"] != 30)].index)
-
-print(data.groupby(["max_iter", "L", "alpha",
-                    "communication_rounds", "epsilon", "tolerance",
-                    "lambda_", "dpMode", "numClients", "foldTest"])\
-                .agg({"accuracy":["mean", "std"], "tolerance": "count"}))
-
-facetgrid = sns.catplot(x="foldTest", y="accuracy", hue="dpMode",
-                        kind="box", col="max_iter", data=data,
-                        palette=sns.color_palette(
-                            [(1.0, 0.4980392156862745, 0.054901960784313725),
-                             (0.12156862745098039, 0.4666666666666667, 0.7058823529411765) ]))
-
-facetgrid.set_xlabels('Fold number')
-facetgrid.set_xticklabels(["1", "2", "3", "4"])
-facetgrid.savefig("foldImpact.png")
-'''
-
-
-### Show the impact of com rounds
-#TODO: change to single colour
+fig.savefig("noDP_vs_dpClient_noDPoptimised.png")
 #'''
-data = df[(df["lambda_"].isin([0.5])) & (df["numClients"].isin([5])) \
-        & (df["dpMode"].isin(["dpClient"])) & (df["max_iter"].isin([500]))]
+
+### Show the impact of com rounds, without clipping
+#'''
+data = df[(df["lambda_"].isin([0.01])) & (df["numClients"].isin([5])) \
+        & (df["dpMode"].isin(["dpClient"])) & (df["max_iter"].isin([500])) \
+        & (df["communication_rounds"].isin(["1_1", "20_20"]))]
 data = data.drop(data[(data["dpMode"] == "dpClient") & (data["epsilon"] != 30)].index)
 
 print(data.groupby(["max_iter", "L", "alpha",
@@ -86,8 +47,112 @@ facetgrid = sns.catplot(x="communication_rounds", y="accuracy",
                         palette=sns.color_palette(
                             [(1.0, 0.4980392156862745, 0.054901960784313725)]))
 
+facetgrid.set_xticklabels(["20", "1"])
 facetgrid.savefig("comRoundImpact.png")
 #'''
 
-### Show the impact of epsilon TODO
+### show that from comRound 1 to 20, no difference in accuracy
+#'''
+data = df[(df["lambda_"].isin([0.01])) & (df["numClients"].isin([5])) \
+        & (df["dpMode"].isin(["dpClient"])) & (df["max_iter"].isin([500])) \
+        & (df["communication_rounds"].isin(["{}_20".format(x) for x in range(1,21)]))]
+data = data.drop(data[(data["dpMode"] == "dpClient") & (data["epsilon"] != 30)].index)
+data["communication_rounds"] = data["communication_rounds"].map(lambda x: int(x.split("_")[0]))
+print(data.groupby(["max_iter", "L", "alpha",
+                    "communication_rounds", "epsilon", "tolerance",
+                    "lambda_", "dpMode", "numClients"])\
+                .agg({"accuracy":["mean", "std"], "tolerance": "count"}))
+print(data.groupby(["max_iter", "L", "alpha",
+                    "communication_rounds", "epsilon", "tolerance",
+                    "lambda_", "dpMode", "numClients"])\
+                .agg({"accuracy":["median", "std"], "tolerance": "count"}))
 
+facetgrid = sns.catplot(x="communication_rounds", y="accuracy",
+                        kind="box", data=data,
+                        palette=sns.color_palette(
+                            [(1.0, 0.4980392156862745, 0.054901960784313725)]))
+
+#facetgrid.set_xticklabels(["20", "1"])
+facetgrid.savefig("comRoundImpactAll20Rounds.png")
+
+#'''
+
+### show where variance comes from
+### 1 communication round, 5 clients, 0.5 for lambda_, eps = 30, all Folds
+#'''
+data = df[(df["lambda_"].isin([0.01])) & (df["numClients"].isin([5])) \
+        & (df["communication_rounds"] == "1_1") & (df["dpMode"].isin(["noDP",
+            "dpClient"])) & (df["max_iter"].isin([500]))]
+data = data.drop(data[(data["dpMode"] == "dpClient") & (data["epsilon"] != 30)].index)
+
+print(data.groupby(["max_iter",
+                    "communication_rounds", "epsilon", "tolerance",
+                    "lambda_", "dpMode", "numClients", "foldTest"])\
+                .agg({"accuracy":["mean", "std"], "tolerance": "count"}))
+
+facetgrid = sns.catplot(x="foldTest", y="accuracy", hue="dpMode",
+                        kind="box", data=data)
+
+facetgrid.set_xlabels('Fold number')
+facetgrid.set_xticklabels(["1", "2", "3", "4"])
+facetgrid.savefig("foldImpact.png")
+#'''
+
+
+
+### Show results with clipping
+#'''
+data = df[(df["lambda_"].isin([0.01])) & (df["numClients"].isin([5])) \
+        & (df["dpMode"].isin(["dpClient", "noDP"])) & (df["max_iter"].isin([2500])) \
+        & (df["communication_rounds"] == "1_1")]
+
+data = data.drop(data[(data["dpMode"] == "dpClient") & (data["epsilon"] != 30)].index)
+data["C"] = data["C"].map(lambda x: float(x))
+print(data.groupby(["max_iter", "alpha",
+                    "communication_rounds", "epsilon", "tolerance",
+                    "lambda_", "dpMode", "numClients", "C"])\
+                .agg({"accuracy":["mean", "std"], "tolerance": "count"}))
+print(data["C"].unique())
+facetgrid = sns.catplot(x="C", y="accuracy",
+                        kind="box", data=data)
+facetgrid.savefig("clippingImpact.png")
+#'''
+
+### Show the impact of comRounds with clipping
+#'''
+data = df[(df["lambda_"].isin([0.01])) & (df["numClients"].isin([5])) \
+        & (df["dpMode"].isin(["dpClient"])) & (df["max_iter"].isin([2500])) \
+        & (df["C"] == 0.5)]
+data = data.drop(data[(data["dpMode"] == "dpClient") & (data["epsilon"] != 30)].index)
+data["communication_rounds"] = data["communication_rounds"].map(lambda x: int(x.split("_")[0]))
+print(data.groupby(["max_iter", "L", "alpha",
+                    "communication_rounds", "epsilon", "tolerance",
+                    "lambda_", "dpMode", "numClients", "C"])\
+                .agg({"accuracy":["mean", "std"], "tolerance": "count"}))
+print(data.groupby(["max_iter", "L", "alpha",
+                    "communication_rounds", "epsilon", "tolerance",
+                    "lambda_", "dpMode", "numClients", "C"])\
+                .agg({"accuracy":["median", "std"], "tolerance": "count"}))
+facetgrid = sns.catplot(x="communication_rounds", y="accuracy",
+                        kind="box", data=data,
+                        palette=sns.color_palette(
+                            [(1.0, 0.4980392156862745, 0.054901960784313725)]))
+
+facetgrid.savefig("comRoundClippingImpact.png")
+#'''
+
+### show the impact of epsilon with clipping
+#'''
+data = df[(df["lambda_"].isin([0.01])) & (df["numClients"].isin([5])) \
+        & (df["dpMode"].isin(["dpClient", "noDP"])) & (df["max_iter"].isin([2500])) \
+        & (df["communication_rounds"] == "1_1") & (df["C"] == "0.5")]
+
+print(data.groupby(["max_iter", "alpha",
+                    "communication_rounds", "epsilon", "tolerance",
+                    "lambda_", "dpMode", "numClients", "C"])\
+                .agg({"accuracy":["mean", "std"], "tolerance": "count"}))
+print(data["C"].unique())
+facetgrid = sns.catplot(x="epsilon", y="accuracy",
+                        kind="box", data=data)
+facetgrid.savefig("clippingImpactNoise.png")
+#'''
